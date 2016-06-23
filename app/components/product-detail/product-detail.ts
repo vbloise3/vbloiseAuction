7bc95e0af36ecc35d51c7bd9b36b5a13549d0745
Vince Bloise <vbloise3@verizon.net>
2016-06-22 20:07:07 -0400

    Added router.dev to index.html

---
+++ app/components/product-detail/product-detail.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by vincebloise on 6/22/16.
+ */
+import {Component} from '@angular/core';
+import {RouteSegment} from '@angular/router';
+
+@Component({
+    selector: 'auction-product-page',
+    template: `
+    <div>
+      <img src="http://placehold.it/820x320">
+      <h4>{{productTitle}}</h4>
+    </div>
+  `
+})
+export default class ProductDetailComponent {
+    productTitle: string;
+
+    constructor(params: RouteSegment){
+        this.productTitle = params.getParam('prodTitle');
+    }
+}
